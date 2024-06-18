153d1a57a61c37e4e1cb03bd08208df9855334be
gerardo-lopez-dev-gh <gerardo.a.lopez1994@gmail.com>
2024-06-17 20:39:45 -0400

    colocamos como estaba antes

--- healthchecks/postgres.sh
+++ healthchecks/postgres.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 set -eo pipefail
 
 host="$(hostname -i || echo '127.0.0.1')"
@@ -7,25 +6,16 @@ user="${POSTGRES_USER:-postgres}"
 db="${POSTGRES_DB:-$POSTGRES_USER}"
 export PGPASSWORD="${POSTGRES_PASSWORD:-}"
 
-# Verificar si el directorio de datos ya existe
-if [ -d "/var/lib/postgresql/data" ]; then
-  echo "Database directory already exists, skipping initialization"
-else
-  echo "Initializing database"
-  initdb -D /var/lib/postgresql/data
-fi
-
-# Verificar la conectividad de la base de datos
 args=(
-  # force postgres to not use the local unix socket (test "external" connectibility)
-  --host "$host"
-  --username "$user"
-  --dbname "$db"
-  --quiet --no-align --tuples-only
+	# force postgres to not use the local unix socket (test "external" connectibility)
+	--host "$host"
+	--username "$user"
+	--dbname "$db"
+	--quiet --no-align --tuples-only
 )
 
 if select="$(echo 'SELECT 1' | psql "${args[@]}")" && [ "$select" = '1' ]; then
-  exit 0
+	exit 0
 fi
 
 exit 1
